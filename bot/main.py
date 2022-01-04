0ae432b1f000faca831eba3f9ceb8d1ab774e546
Ysora <EvanWrynn@gmail.com>
2022-01-04 23:55:31 +0000

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -288,20 +288,15 @@ async def on_message(message):
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠛⠋⠉⠉⠁⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠁""")
 
-# reply if a message contains a phase 
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
-    if message.content.startswith("hotel motel"):
-        await message.channel.send("HOLIDAY INN")
 
 # change profile picture
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    if message.content.startswith("change profile picture"):
-        await bot.user.edit(avatar=discord.File("https://cdn.discordapp.com/attachments/873667988363046947/926486757254524948/271130049_5380860148608730_7329237292142343107_n.png"))
-    
+    if message.content.startswith("https://c.tenor.com/OXevc9fjZmgAAAAM/tasting-milk-antony-starr.gif"):
+        await message.delete()
+    if message.content.startswith("hotel motel"):
+        await message.channel.send("HOLIDAY INN")
 # command to dm a user
 @bot.command()
 async def dm(ctx, user: discord.Member, *, message):
